f55203a2869b9467453b0209ed2325d3b3e0ac8e
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-01-03 17:19:41 +0100

    implemented yarl instead of urllib

--- scrape-pizza-data/src/scraper/settings/__init__.py
+++ scrape-pizza-data/src/scraper/settings/__init__.py
@@ -1,1 +1,9 @@
 """Modules containing scraper settings."""
+
+from scraper.settings.source import SourcePageSettings
+
+source_page = SourcePageSettings()
+
+__all__ = [
+    "source_page",
+]
--- scrape-pizza-data/src/scraper/settings/source.py
+++ scrape-pizza-data/src/scraper/settings/source.py
@@ -1,20 +1,20 @@
 """Settings for the source location to be scraped."""
 
-import urllib
 from typing import Annotated
 
 import pydantic
 import pydantic_settings
+import yarl
 
 
 class SourcePageSettings(pydantic_settings.BaseSettings):
     """Settings for the source webpage location to be scraped."""
 
     url_home: Annotated[
-        urllib.URL, pydantic.Field(
+        yarl.URL, pydantic.Field(
             description="The URL of the source to be scraped."
         ),
-    ] = urllib.URL("https://www.50toppizza.it/")
+    ] = yarl.URL("https://www.50toppizza.it/")
     selection_type: Annotated[
         str,
         pydantic.Field(
