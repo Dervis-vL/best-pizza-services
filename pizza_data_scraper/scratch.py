69eb744b1e47865a75175c12bd77a1a05b321715
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-01-19 22:08:27 +0100

    updated scratch

--- pizza_data_scraper/scratch.py
+++ pizza_data_scraper/scratch.py
@@ -18,14 +18,18 @@ if __name__ == "__main__":
 
     if WRITE_TO_FILE:
         # Write to file
-        utils.soup_to_file(scraped_ranked_data, HTML_OUTPUT_PATH)
+        if not HTML_OUTPUT_PATH.exists():
+            utils.soup_to_file(scraped_ranked_data, HTML_OUTPUT_PATH)
 
     # Scrape pizzeria data from cards
     cards = scraped_ranked_data.select(CARDS_SELECTOR)
     for i, card in enumerate(cards):
+        name = card.select_one("h3").get_text(strip=True).lower().replace(" ", "_")
         pizzeria_soup = logic.get_scraped_pizzeria_data(card)
 
         if WRITE_TO_FILE:
             # Write to file
-            pizzeria_output_path = ROOT_PATH / "dev" / "HTML" / f"pizzeria_{location.name}_{year.value}_{i+1}.html"
-            utils.soup_to_file(pizzeria_soup, pizzeria_output_path)
+            pizzeria_output_path = ROOT_PATH / "dev" / "HTML" / f"pizzeria_{name}.html"
+            # Check if file exists
+            if not pizzeria_output_path.exists():
+                utils.soup_to_file(pizzeria_soup, pizzeria_output_path)
