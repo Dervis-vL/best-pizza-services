0f744f5178bb643a407abdc118ead5b9a7278d6e
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-02-08 19:56:43 +0100

    update init

--- pizza_data_scraper/src/pizza_data_scraper/models/__init__.py
+++ pizza_data_scraper/src/pizza_data_scraper/models/__init__.py
@@ -1,9 +1,11 @@
 """Module for pizza data scraper models."""
 
+from pizza_data_scraper.models.base import BaseModel
 from pizza_data_scraper.models.categories import Categories
 from pizza_data_scraper.models.ranking_editions import RankingEditions
 
 __all__ = [
+    "BaseModel",
     "Categories",
     "RankingEditions",
 ]
