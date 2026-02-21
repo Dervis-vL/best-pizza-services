659b01583435e205b9a89a30bdba25957335a954
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-02-21 22:18:39 +0100

    first app function

--- pizza_app/src/pizza_app/app.py
+++ pizza_app/src/pizza_app/app.py
@@ -4,9 +4,10 @@ from __future__ import annotations
 
 import streamlit as st
 import folium
+from pandera import typing as pa_typing
 from streamlit_folium import st_folium
 
-from pizza_app import repositories, settings
+from pizza_app import repositories, settings, schemas
 
 
 # Page CONFIG
@@ -18,3 +19,10 @@ st.set_page_config(
 
 st.title("🍕 Pizzerias — World Map")
 st.caption("Locations sourced from 50 Top Pizza rankings.")
+
+# Data LOAD
+@st.cache_data(ttl=300, show_spinner="Loading Pizzerias from DB...")
+def load_locations() -> pa_typing.DataFrame[schemas.PizzeriaSchema]:
+    """Load data from db."""
+    pizza_repo = repositories.PizzaPlatformDatabase(settings=settings.pizza_db)
+    return pizza_repo.read()
