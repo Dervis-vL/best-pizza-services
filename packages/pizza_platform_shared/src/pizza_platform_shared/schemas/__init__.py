43b5ced91f3bd9c9180403f531dd9459dc469da6
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-03-19 07:58:27 +0100

    moved some schemas

--- packages/pizza_platform_shared/src/pizza_platform_shared/schemas/__init__.py
+++ packages/pizza_platform_shared/src/pizza_platform_shared/schemas/__init__.py
@@ -1,1 +1,9 @@
 """Pizza platform pandera schemas."""
+
+from pizza_platform_shared.schemas.category import CategorySchema
+from pizza_platform_shared.schemas.endpoints import (
+    RankingEndpointsSchema,
+    PizzeriaEndpointsSchema,
+)
+from pizza_platform_shared.schemas.locations import LocationSchema
+from pizza_platform_shared.schemas.pizzeria import PizzeriaSchema
--- services/pizza_data_management/src/pizza_data_management/schemas/__init__.py
+++ services/pizza_data_management/src/pizza_data_management/schemas/__init__.py
@@ -1,15 +1,15 @@
 """Schemas for the pizza data scraper."""
 
-from pizza_data_management.schemas.category import CategorySchema
-from pizza_data_management.schemas.locations import LocationSchema
-from pizza_data_management.schemas.pizzeria import PizzeriaSchema
-from pizza_data_management.schemas.ranked_edition import RankedEditionSchema
-from pizza_data_management.schemas.ranking_position import RankingPositionSchema
-from pizza_data_management.schemas.endpoints import (
+from pizza_platform_shared.schemas import (
+    CategorySchema,
+    LocationSchema,
+    PizzeriaSchema,
+    RankedEditionSchema,
+    RankingPositionSchema,
     RankingEndpointsSchema,
-    PizzeriaEndpointsSchema
+    PizzeriaEndpointsSchema,
+    WebpagesSchema,
 )
-from pizza_data_management.schemas.webpages import WebpagesSchema
 
 __all__ = [
     "CategorySchema",
