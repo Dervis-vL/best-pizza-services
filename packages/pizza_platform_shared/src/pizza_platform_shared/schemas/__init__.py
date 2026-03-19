f81ae84daa10dbb6a46f46c187e445a9f04010b5
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-03-19 16:36:51 +0100

    moved schemas

--- packages/pizza_platform_shared/src/pizza_platform_shared/schemas/__init__.py
+++ packages/pizza_platform_shared/src/pizza_platform_shared/schemas/__init__.py
@@ -7,3 +7,17 @@ from pizza_platform_shared.schemas.endpoints import (
 )
 from pizza_platform_shared.schemas.locations import LocationSchema
 from pizza_platform_shared.schemas.pizzeria import PizzeriaSchema
+from pizza_platform_shared.schemas.ranked_edition import RankedEditionSchema
+from pizza_platform_shared.schemas.ranking_position import RankingPositionSchema
+from pizza_platform_shared.schemas.webpages import WebpagesSchema
+
+__all__ = [
+    "CategorySchema",
+    "LocationSchema",
+    "RankedEditionSchema",
+    "RankingEndpointsSchema",
+    "RankingPositionSchema",
+    "PizzeriaEndpointsSchema",
+    "PizzeriaSchema",
+    "WebpagesSchema",
+]
