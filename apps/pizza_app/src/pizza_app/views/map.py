4308f0ebe50fdbefc5dd1980b4ea735c11d7f033
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-03-24 17:33:58 +0100

    gps to map code

--- apps/pizza_app/src/pizza_app/views/map.py
+++ apps/pizza_app/src/pizza_app/views/map.py
@@ -62,4 +62,5 @@ def render_map(
             icon=folium.Icon(color="red", icon="cutlery", prefix="fa"),
         ).add_to(cluster)
 
+    fo_plugins.LocateControl(auto_start=False, initialZoomLevel=10).add_to(m)
     st_folium(m, width="stretch", height=650, returned_objects=[])
