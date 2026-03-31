2dbb6e5bc7aabf7740dd92d8e33cbc202cb5f377
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-03-31 07:59:52 +0200

    first run after refactor storage

--- services/pizza_data_storage/src/pizza_data_storage/application/use_cases/__init__.py
+++ services/pizza_data_storage/src/pizza_data_storage/application/use_cases/__init__.py
@@ -1,1 +1,7 @@
 """Use cases for pizza data storage service."""
+
+from pizza_data_storage.application.use_cases.seed_categories_and_editions import (
+    SeedCategoriesAndEditionsUseCase
+)
+
+__all__ = ["SeedCategoriesAndEditionsUseCase"]
--- services/pizza_data_storage/src/pizza_data_storage/application/use_cases/seed_categories_and_editions.py
+++ services/pizza_data_storage/src/pizza_data_storage/application/use_cases/seed_categories_and_editions.py
@@ -1,1 +1,18 @@
 """Seed categories and editions use case."""
+
+from pizza_platform_shared import schemas as shared_schemas
+
+from pizza_data_storage.application import ports
+
+
+class SeedCategoriesAndEditionsUseCase:
+    """Seed categories and editions use case."""
+
+    def __init__(self, ranking_repository: ports.IRankingsRepository):
+        self._ranking_repository = ranking_repository
+
+    def execute(self, config_schema: shared_schemas.RankedCategoriesSchema):
+        """Execute the use case."""
+        self._ranking_repository.seed_categories_and_editions(
+            config_schema=config_schema
+        )
