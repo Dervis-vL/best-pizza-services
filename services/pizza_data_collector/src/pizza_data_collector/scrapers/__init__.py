0836b8ca267f6471807683903aa5492fe49beabe
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-04-11 13:10:59 +0200

    some bug fixed

--- services/pizza_data_collector/src/pizza_data_collector/scrapers/__init__.py
+++ services/pizza_data_collector/src/pizza_data_collector/scrapers/__init__.py
@@ -1,8 +1,6 @@
 """Scrapers module for pizza data collection."""
 
 from pizza_data_collector.scrapers.http_client import HttpClient
-from services.pizza_data_collector.src.pizza_data_collector.scrapers.scraper import (
-    Scraper,
-)
+from pizza_data_collector.scrapers.scraper import Scraper
 
 __all__ = ["Scraper", "HttpClient"]
