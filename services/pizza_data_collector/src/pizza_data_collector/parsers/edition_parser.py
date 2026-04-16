4fe4e7e1655bcf046809fddbe8c6bd753df909fc
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-04-16 16:23:45 +0200

    refactored ediotn page a bit

--- services/pizza_data_collector/src/pizza_data_collector/parsers/edition_parser.py
+++ services/pizza_data_collector/src/pizza_data_collector/parsers/edition_parser.py
@@ -1,18 +1,9 @@
 """Edition parser implementation for pizza data collector service."""
 
 from bs4 import BeautifulSoup
-from pizza_platform_shared.schemas import (
-    PizzeriaSchema,
-    RankingSchema,
-    WebpagesSchema,
-)
+from pizza_platform_shared import schemas
 
-from pizza_data_collector import utils
-from pizza_data_collector.models.parser.patterns import (
-    CardPatterns,
-    RankingPositionPatterns,
-    URLPatterns,
-)
+from pizza_data_collector import models, utils
 
 
 class EditionParser:  # pylint: disable=too-few-public-methods
@@ -20,20 +11,20 @@ class EditionParser:  # pylint: disable=too-few-public-methods
 
     def __init__(
         self,
-        card_patterns: CardPatterns,
-        url_pattern: URLPatterns,
-        position_patterns: RankingPositionPatterns,
+        card_patterns: models.CardPatterns,
+        url_pattern: models.URLPatterns,
+        position_patterns: models.RankingPositionPatterns,
     ) -> None:
         """Initializes the edition parser with the necessary patterns."""
         self._cards = card_patterns
         self._urls = url_pattern
         self._positions = position_patterns
 
-    def parse(self, soup: BeautifulSoup, edition_id: int) -> list[PizzeriaSchema]:
+    def parse(self, soup: BeautifulSoup, edition_id: int) -> list[schemas.PizzeriaSchema]:
         """Parses the edition page soup and returns structured data."""
         html_cards_list = self._cards.extract(html=str(soup))
 
-        pizzerias: dict[str, PizzeriaSchema] = {}
+        pizzerias: dict[str, schemas.PizzeriaSchema] = {}
         for card in html_cards_list:
             url = self._urls.extract(html=card)
             if url is None:
@@ -43,15 +34,15 @@ class EditionParser:  # pylint: disable=too-few-public-methods
             position = self._positions.extract(html=card)
 
             if slug not in pizzerias:
-                pizzerias[slug] = PizzeriaSchema(
+                pizzerias[slug] = schemas.PizzeriaSchema(
                     slug=slug,
-                    rankings=[RankingSchema(position=position, edition_id=edition_id, awards=None)],
-                    webpages=[WebpagesSchema(url=url, slug=slug)],
+                    rankings=[schemas.RankingSchema(position=position, edition_id=edition_id, awards=None)],
+                    webpages=[schemas.WebpagesSchema(url=url, slug=slug)],
                 )
             else:
                 pizzerias[slug].rankings.append(
-                    RankingSchema(position=position, edition_id=edition_id, awards=None)
+                    schemas.RankingSchema(position=position, edition_id=edition_id, awards=None)
                 )
-                pizzerias[slug].webpages.append(WebpagesSchema(url=url, slug=slug))
+                pizzerias[slug].webpages.append(schemas.WebpagesSchema(url=url, slug=slug))
 
         return list(pizzerias.values())
