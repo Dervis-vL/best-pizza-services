ec1f3371f640b3b4bb0b666dc3a2db1aa6974dc9
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-04-21 22:52:08 +0200

    added storage settings

--- services/pizza_data_storage/src/pizza_data_storage/settings/__init__.py
+++ services/pizza_data_storage/src/pizza_data_storage/settings/__init__.py
@@ -3,11 +3,14 @@
 from pizza_platform_shared.settings import PizzaDatabaseSettings
 
 from pizza_data_storage.settings.maintenance_database import MaintenanceDatabaseSettings
+from pizza_data_storage.settings.storage import PizzaStorageSettings
 
 __all__ = [
     "MaintenanceDatabaseSettings",
     "PizzaDatabaseSettings",
+    "PizzaStorageSettings",
 ]
 
-pizza_db = PizzaDatabaseSettings()
 maintenance_db = MaintenanceDatabaseSettings()
+pizza_db = PizzaDatabaseSettings()
+pizza_storage = PizzaStorageSettings()
