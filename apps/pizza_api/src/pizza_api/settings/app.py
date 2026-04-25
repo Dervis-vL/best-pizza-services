a49e22838a9c9d3a9fa64a5fb5ee7a642044f610
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-04-25 16:14:55 +0200

    some app settings improvements

--- apps/pizza_api/src/pizza_api/settings/app.py
+++ apps/pizza_api/src/pizza_api/settings/app.py
@@ -1,14 +1,18 @@
 """App settings for the pizza API."""
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Annotated
+
+import pydantic as pyd
+import pydantic_settings as pyd_settings
+from pizza_platform_shared import settings as shared_settings
 
 from pizza_api import __version__, __description__
 
 
-class ApplicationSettings(BaseSettings):
+class ApplicationSettings(pyd_settings.BaseSettings):
     """Application settings for the pizza API."""
 
-    model_config = SettingsConfigDict(
+    model_config = pyd_settings.SettingsConfigDict(
         env_prefix="PIZZA_API_",
         env_file=".env",
         env_file_encoding="utf-8",
@@ -17,6 +21,8 @@ class ApplicationSettings(BaseSettings):
     title: str = "Best Pizza API"
     description: str = __description__
     version: str = __version__
-    host: str = ""
-    port: int = 8000
+    pizza_db: Annotated[
+        shared_settings.PizzaDatabaseSettings,
+        pyd.Field(default_factory=shared_settings.PizzaDatabaseSettings)
+    ] = shared_settings.pizza_db
     debug: bool = False
