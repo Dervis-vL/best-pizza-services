2cad0c60ee9e5fd8aa9b7fd00b43c71ad92a3028
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-05-22 13:04:47 +0200

    added router

---
+++ apps/pizza_api/src/pizza_api/routers/pizzerias.py
@@ -0,0 +1,28 @@
+"""Router for getting pizzeria data endpoints."""
+
+import logging
+from typing import Annotated
+
+from fastapi import APIRouter, Query, status
+
+from pizza_api import dependencies
+from pizza_platform_shared import enums as shared_enums
+from pizza_platform_shared import schemas as shared_schemas
+
+router = APIRouter(prefix="/pizzerias", tags=["pizzerias"])
+
+_pizza_api_logger = logging.getLogger("pizza_api")
+
+
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[shared_schemas.PizzeriaReadSchema],
+    summary="Get the Pizzerias data from the database.",
+)
+def get_pizzerias(
+    use_case: dependencies.ReadPizzeriasUCDep,
+    include: Annotated[list[shared_enums.PizzeriaInclude] | None, Query()] = None,
+) -> list[shared_schemas.PizzeriaReadSchema]:
+    """Get all the pizzeria data from the DB, and optionally include relationships."""
+    return use_case.execute(relationships=include or [])
