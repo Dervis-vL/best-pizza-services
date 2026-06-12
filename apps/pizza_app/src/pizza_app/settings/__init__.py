db0f79cbba7785de5cd608c18bb87675b478580f
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-06-12 12:37:55 +0200

    removed db type from app

--- apps/pizza_app/src/pizza_app/settings/__init__.py
+++ apps/pizza_app/src/pizza_app/settings/__init__.py
@@ -1,10 +1,8 @@
 """Settings for the pizza app."""
 
-from pizza_app.settings.application import Application
 from pizza_app.settings.pizza_api import PizzaAPISettings
 from pizza_platform_shared.settings import pizza_db
 
-app_settings = Application()
 pizza_api = PizzaAPISettings()
 
-__all__ = ["app_settings", "pizza_api", "pizza_db"]
+__all__ = ["pizza_api", "pizza_db"]
