fec3fbc7d951cd5b484e81d52088f5ecede0ef1d
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-06-25 22:13:15 +0200

    implemented api pool config

--- apps/pizza_api/src/pizza_api/settings/__init__.py
+++ apps/pizza_api/src/pizza_api/settings/__init__.py
@@ -1,7 +1,9 @@
 """Settings for the pizza API."""
 
 from pizza_api.settings.app import ApplicationSettings
+from pizza_api.settings.engine_pool import ApiPoolSettings
 
 app_settings = ApplicationSettings()
+pool = ApiPoolSettings()
 
-__all__ = ["app_settings"]
+__all__ = ["app_settings", "pool"]
