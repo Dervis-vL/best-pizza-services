70053ffd27d8d18739d838e312902b2ad1a79bb7
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-07-02 16:57:08 +0200

    adde staging table repo port

--- services/pizza_data_storage/src/pizza_data_storage/application/ports/__init__.py
+++ services/pizza_data_storage/src/pizza_data_storage/application/ports/__init__.py
@@ -1,7 +1,15 @@
 """Ports layer for pizza data storage application."""
 
+from pizza_data_storage.application.ports.category_payload_repository import (
+    ICategoryPayloadRepository,
+)
 from pizza_data_storage.application.ports.html_repository import IHtmlStorageRepository
 from pizza_data_storage.application.ports.pizzeria_repository import IPizzeriaRepository
 from pizza_data_storage.application.ports.ranking_repository import IRankingsRepository
 
-__all__ = ["IHtmlStorageRepository", "IPizzeriaRepository", "IRankingsRepository"]
+__all__ = [
+    "ICategoryPayloadRepository",
+    "IHtmlStorageRepository",
+    "IPizzeriaRepository",
+    "IRankingsRepository",
+]
